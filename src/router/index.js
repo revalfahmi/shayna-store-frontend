df7bc49a2d5ed50c62b854c618ae77625e335eed
revalfahmi <reval.fahmi42@gmail.com>
2023-06-29 23:26:48 +0700

    Payment view and Success View

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ProductView from '../views/ProductView.vue'
+import ShoppingCart from '../views/ShoppingCart.vue'
+import SuccessView from '../views/SuccessView.vue'
 
 Vue.use(VueRouter)
 
@@ -23,6 +25,16 @@ const routes = [
     path: '/product',
     name: 'product',
     component: ProductView
+  },
+  {
+    path: '/cart',
+    name: 'cart',
+    component: ShoppingCart
+  },
+  {
+    path: '/success',
+    name: 'success',
+    component: SuccessView
   }
 ]
 
